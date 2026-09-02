6230b7c4a039f5f156fa2ee1203492dfafb66636
Marian Mocanu <mocanu.marian@agilefreaks.com>
2026-09-02 10:41:31 +0300

    chore: version 1.0.2 (versionCode 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "dev.ceireader.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
